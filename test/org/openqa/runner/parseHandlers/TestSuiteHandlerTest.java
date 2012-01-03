9db5133a0b0a8368ef0efc561baad5958eddac64
Aleksey <toseter.lex@gmail.com>
2012-01-03 22:30:21 +0400

    done TestSuiteHandler.java tests

--- test/org/openqa/runner/parseHandlers/TestSuiteHandlerTest.java
+++ test/org/openqa/runner/parseHandlers/TestSuiteHandlerTest.java
@@ -19,6 +19,13 @@ import junit.framework.JUnit4TestAdapter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.runner.parserHandlers.TestSuiteHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class TestSuiteHandlerTest {
@@ -35,6 +42,28 @@ public class TestSuiteHandlerTest {
     }
 
     @Test
-    public void testHandle() {
+    public void testHandle() throws Exception {
+
+        final String testFile = "testData" + File.separator + "testSuite.ts";
+
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+
+        TestSuiteHandler handler = new TestSuiteHandler();
+
+        SAXParser saxParser = spf.newSAXParser();
+        saxParser.parse(testFile, handler);
+
+
+        String[] tests = handler.getTests();
+
+        assertEquals(2, tests.length);
+        assertEquals("firstTest.t", tests[0]);
+        assertEquals("secondTest.t", tests[1]);
+
+        assertEquals("fixture.f", handler.getBeforeSuite());
+        assertEquals("fixture.f", handler.getAfterSuite());
+
+        assertEquals("Test Suite", handler.getTitle());
+
     }
 }
